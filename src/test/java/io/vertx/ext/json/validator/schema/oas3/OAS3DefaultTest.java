96ffe0805940f60a14d539b083ad415e53139594
Francesco Guardiani <francescoguard@gmail.com>
2018-03-30 17:12:40 +0200

    Wrote test for new JsonPointer error location and additionalProperties/default combination
    Signed-off-by: Francesco Guardiani <francescoguard@gmail.com>

--- src/test/java/io/vertx/ext/json/validator/schema/oas3/OAS3DefaultTest.java
+++ src/test/java/io/vertx/ext/json/validator/schema/oas3/OAS3DefaultTest.java
@@ -2,6 +2,7 @@ package io.vertx.ext.json.validator.schema.oas3;
 
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
+import io.vertx.core.json.JsonPointer;
 import io.vertx.ext.json.validator.ValidationException;
 import io.vertx.ext.json.validator.WrongSchemaException;
 import io.vertx.ext.json.validator.schema.AllOfSchema;
@@ -50,6 +51,44 @@ public class OAS3DefaultTest extends OAS3ManualTest {
             testJsonParameter(result, "/a/a", String.class, "deep_a");
             testJsonParameter(result, "/b/b", String.class, "deep_b");
             testJsonParameter(result, "/c", String.class, "c");
+        }));
+    }
+
+    @Test
+    public void additionalPropertiesDeep(TestContext context) throws IOException {
+        Schema<JsonObject> s = loadSchema("additionalPropertiesDeepDefault.json");
+        assertThat(s).isInstanceOf(OAS3ObjectSchema.class);
+        s.validate(new JsonObject()).setHandler(context.asyncAssertSuccess());
+        s.validate(
+                new JsonObject().put("a",
+                        new JsonObject().put("b",
+                                new JsonObject().put("c2", "bla")
+                        )
+                )).setHandler(context.asyncAssertSuccess(res -> {
+            testJsonParameter(res, "/a/b/c2", String.class);
+            testJsonParameter(res, "/a/b/c1", String.class, "c1");
+        }));
+        s.validate(
+                new JsonObject().put("a",
+                        new JsonObject().put("b",
+                                new JsonObject()
+                        )
+                )).setHandler(context.asyncAssertFailure(throwable -> {
+                    assertThat(throwable)
+                            .isInstanceOf(ValidationException.class)
+                            .hasFieldOrPropertyWithValue("pointer", JsonPointer.from("/a/b"));
+        }));
+        s.validate(
+                new JsonObject().put("a",
+                        new JsonObject().put("b",
+                                new JsonObject()
+                                        .put("c2", "bla")
+                                        .put("c3", new JsonObject())
+                        )
+                )).setHandler(context.asyncAssertFailure(throwable -> {
+                    assertThat(throwable)
+                        .isInstanceOf(ValidationException.class)
+                        .hasFieldOrPropertyWithValue("pointer", JsonPointer.from("/a/b/c3"));
         }));
     }
 
